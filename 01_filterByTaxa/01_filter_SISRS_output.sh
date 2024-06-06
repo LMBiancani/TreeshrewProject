6c644c7ba9b757b091eb276f0e16ba28fa10ebc4
Leann Biancani <biancani@ssh3.hac.uri.edu>
2024-06-06 11:58:10 -0400

    Update/Fix filterByTaxa sh and py files, add necessary groups.csv file

--- 01_filterByTaxa/01_filter_SISRS_output.sh
+++ 01_filterByTaxa/01_filter_SISRS_output.sh
@@ -1,43 +1,32 @@
 #!/bin/sh
-
 #SBATCH --job-name="filter"
-#SBATCH --time=24:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=36:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # processor core(s) per node - just use 1 
 #SBATCH --mail-user="biancani@uri.edu" #CHANGE user email address
 #SBATCH --mail-type=ALL
-### adjust/add sbatch flags as needed
+
+## UPDATE PATHS ...
+# path to TreeshrewProject Directory:
+PROJECT=/data/schwartzlab/Biancani/TreeshrewProject
+# path to output folder (will be created if doesn't exist):
+OUTFOLDER=$PROJECT/output/FilteredByTaxa
+# path to SISRS_Run directory (contains SISRS output from step 00):
+SISRS_Run=$PROJECT/SISRS_Run
+# path to Groups List (csv file containing taxon to group correspondence list):
+TAXONGROUPS=$PROJECT/01_filterByTaxa/groups.csv
+
+## SPECIFY PARAMETERS ...
+SEQCOMPLETE=0.33 # Minimum Sequence Completeness (e.g. 0.33 is at least 33% non N)
+MINTAXA=25 # Minimum Number of Taxa Retained (e.g. locus passes if 25 taxa are remaining)
+MINGROUPS=6 # Minimum Number of Groups Present (e.g. all 6 groups must be present)
 
 cd $SLURM_SUBMIT_DIR
 
 module purge
-
 #for URI's Andromeda cluster
 module load Biopython/1.78-foss-2020b
 
-## Update the following paths as needed...
-
-## path to TreeshrewProject Directory:
-PROJECT=/data/schwartzlab/Biancani/TreeshrewProject
-
-## path to output folder (will be created if doesn't exist):
-OUTFOLDER=$PROJECT/output/FilteredByTaxa
-
-## path to Taxon List (csv file containing taxon to group correspondence list):
-TAXA=$PROJECT/TaxonList.txt
-
-## path to SISRS_Run directory (contains SISRS output from step 00):
-SISRS_Run=$PROJECT/SISRS_run
-
-## Specify the following parameters...
-
-## Sequence Completeness Threshold (e.g. 0.33 indicates each passing sequence must have over 33% non N)
-COMPLETENESS=0.33
-
-## Number of Taxa Retained (e.g. 25 means the locus passes if 25 taxa are remaining)
-TAXA_PRESENT=25
-
-## Number of Groups Present (e.g. for the treeshrew project all 6 groups must be present)
-GROUPS_PRESENT=6
-
 mkdir -p $OUTFOLDER
-python $PROJECT/01_filterByTaxa/filter_SISRS_output.py $TAXA $SISRS_Run/aligned_contigs $OUTFOLDER $COMPLETENESS $TAXA_PRESENT $GROUPS_PRESENT
+python $PROJECT/01_filterByTaxa/filter_SISRS_output.py $TAXONGROUPS $SISRS_Run/aligned_contigs $OUTFOLDER $SEQCOMPLETE $MINTAXA $MINGROUPS
 
